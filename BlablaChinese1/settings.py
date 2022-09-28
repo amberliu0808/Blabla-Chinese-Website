d51f2146ff372a35906983dceeb27e6b0c48d754
刘亚丹 <liuyadan@liuyadandeMacBook-Air.local>
2022-09-28 13:52:33 +0800

    remove cors heasers

--- BlablaChinese1/settings.py
+++ BlablaChinese1/settings.py
@@ -44,13 +44,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'website',
     'owner',
-    'corsheaders'
     # 'ckeditor',
 
 ]
 
 MIDDLEWARE = [
-    '**corsheaders.middleware.CorsMiddleware**',
     'django.middleware.security.SecurityMiddleware',
     "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
