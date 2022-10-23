7908083f0977390227b498283ae4f163a8a79fa1
Amber Liu <amberliu0808@foxmail.com>
2022-10-23 16:55:19 +0800

    .env production

--- BlablaChinese1/settings.py
+++ BlablaChinese1/settings.py
@@ -77,7 +77,7 @@ ROOT_URLCONF = 'BlablaChinese1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
