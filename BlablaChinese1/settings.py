2f95f8905acdd37ac6de163dc175c7f6e62edf20
Amber Liu <amberliu0808@foxmail.com>
2022-10-23 17:12:04 +0800

    cancel HSTS

--- BlablaChinese1/settings.py
+++ BlablaChinese1/settings.py
@@ -39,10 +39,10 @@ CSRF_TRUSTED_ORIGINS = ['https://blablachinese.com', 'https://staging.blablachin
                         'https://web-staging-4e49.up.railway.app', 'https://127.0.0.1']
 
 # only active this to True when in Production
-SECURE_HSTS_SECONDS = 31536000
+# SECURE_HSTS_SECONDS = 31536000
 # SECURE_HSTS_SECONDS = None
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-SECURE_HSTS_PRELOAD = True
+# SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+# SECURE_HSTS_PRELOAD = True
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
